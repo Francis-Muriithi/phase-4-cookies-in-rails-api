fb062165b45e70ff11ab7a05921e349c08e014bf
Francis-Muriithi <mufraggie@gmail.com>
2022-11-28 13:20:14 +0300

    complete

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   
   def index
     session[:session_hello] ||= "World"
-    cookies[:cookies_hello] ||= "World"
+    cookies[:cookies_hello] ||= "Worlder"
     render json: { session: session, cookies: cookies.to_hash }
   end
 
